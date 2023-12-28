cd589e28449891b4f25e336d6dac021582cc3129
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2023-12-28 14:23:58 +1000

    add cancellation tokens

--- TempusApi/ITempusClient.cs
+++ TempusApi/ITempusClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using TempusApi.Enums;
 using TempusApi.Models;
@@ -18,7 +19,7 @@ public interface ITempusClient
     /// /activity
     /// Returns recent records and top times.
     /// </summary>
-    Task<RecentActivityModel> GetRecentActivityAsync();
+    Task<RecentActivityModel> GetRecentActivityAsync(CancellationToken cancellationToken = default);
 
     #endregion
 
@@ -29,7 +30,7 @@ public interface ITempusClient
     /// Returns a demo overview.
     /// </summary>
     /// <param name="demoId">The demo ID. A working example value is 2294289.</param>
-    Task<ServerDemoFullOverview> GetDemoInfoAsync(long demoId);
+    Task<ServerDemoFullOverview> GetDemoInfoAsync(long demoId, CancellationToken cancellationToken = default);
 
     #endregion
 
@@ -39,21 +40,21 @@ public interface ITempusClient
     /// /maps/detailedList
     /// List maps on Tempus, with detailed information
     /// </summary>
-    Task<List<DetailedMapOverviewModel>> GetDetailedMapListAsync();
+    Task<List<DetailedMapOverviewModel>> GetDetailedMapListAsync(CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/id/{mapId}/fullOverview
     /// Get information on a map by its ID
     /// </summary>
     /// <param name="mapId">The map ID. A working example value is 1.</param>
-    Task<MapFullOverviewModel> GetFullMapOverviewAsync(long mapId);
+    Task<MapFullOverviewModel> GetFullMapOverviewAsync(long mapId, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/id/{mapId}/fullOverview2
     /// Get more information on a map by its ID
     /// </summary>
     /// <param name="mapId">The map ID. A working example value is 1.</param>
-    Task<FullMapOverview2> GetFullMapOverview2Async(long mapId);
+    Task<FullMapOverview2> GetFullMapOverview2Async(long mapId, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/id/{mapId}/zones/typeindex/{zoneType}/{zoneIndex}/records/list
@@ -66,7 +67,7 @@ public interface ITempusClient
     /// <param name="start">The record to start at. The default is 1.</param>
     /// <param name="sort">The field to sort on the. Working value is "duration"</param>
     /// <param name="direction">The direction to sort. Working values are "ascending"</param>
-    Task<ZonedRecordsModel> GetTopZonedTimes(long mapId, ZoneType zoneType, int zoneIndex = 1, int? limit = null, int? start = null, string sort = null, SortDirection? direction = null);
+    Task<ZonedRecordsModel> GetTopZonedTimes(long mapId, ZoneType zoneType, int zoneIndex = 1, int? limit = null, int? start = null, string sort = null, SortDirection? direction = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// /maps/id/{mapId}/zones/typeindex/{zoneType}/{zoneIndex}/records/player/{playerId}/{class}
@@ -77,34 +78,34 @@ public interface ITempusClient
     /// <param name="zoneIndex">The zone index, starting from 1. This number is referred to in names such as "bonus 1" and "course 3". For map zone records, it is always 1.</param>
     /// <param name="playerId">The player ID.</param>
     /// <param name="playerClass">The class. 3 is soldier, 4 is demoman.</param>
-    Task<PersonalRecordsModel> GetPersonalRecordAsync(long mapId, ZoneType zoneType, int zoneIndex, long playerId, Class playerClass);
+    Task<PersonalRecordsModel> GetPersonalRecordAsync(long mapId, ZoneType zoneType, int zoneIndex, long playerId, Class playerClass, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/list
     /// List maps on Tempus
     /// </summary>
-    Task<List<ShortMapInfoModel>> GetMapListAsync();
+    Task<List<ShortMapInfoModel>> GetMapListAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
     /// /maps/name/{mapName}/fullOverview
     /// Get information on a map by its ID
     /// </summary>
     /// <param name="mapName">The map's name. A working example value is "jump_beef".</param>
-    Task<MapFullOverviewModel> GetFullMapOverviewAsync(string mapName);
+    Task<MapFullOverviewModel> GetFullMapOverviewAsync(string mapName, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/name/{mapName}/fullOverview2
     /// Get more information on a map by its ID
     /// </summary>
     /// <param name="mapName">The map's name. A working example value is "jump_beef".</param>
-    Task<FullMapOverview2> GetFullMapOverview2Async(string mapName);
+    Task<FullMapOverview2> GetFullMapOverview2Async(string mapName, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/name/{mapName}/wrs
     /// Get world record run data for a map
     /// </summary>
     /// <param name="mapName">The map name. A working example value is "jump_beef".</param>
-    Task<WorldRecordsData> GetWorldRecordsDataAsync(string mapName);
+    Task<WorldRecordsData> GetWorldRecordsDataAsync(string mapName, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/name/{mapName}/zones/typeindex/{zoneType}/{zoneIndex}/records/list
@@ -117,7 +118,7 @@ public interface ITempusClient
     /// <param name="start">The record to start at. The default is 1.</param>
     /// <param name="sort">The field to sort on the. Working value is "duration"</param>
     /// <param name="direction">The direction to sort. Working values are "ascending"</param>
-    Task<ZonedRecordsModel> GetTopZonedTimes(string mapName, ZoneType zoneType, int zoneIndex = 1, int? limit = null, int? start = null, string sort = null, string direction = null);
+    Task<ZonedRecordsModel> GetTopZonedTimes(string mapName, ZoneType zoneType, int zoneIndex = 1, int? limit = null, int? start = null, string sort = null, string direction = null, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /maps/name/{mapName}/zones/typeindex/{zoneType}/{zoneIndex}/records/player/{playerId}/{class}
@@ -128,7 +129,7 @@ public interface ITempusClient
     /// <param name="zoneIndex">The zone index, starting from 1. This number is referred to in names such as "bonus 1" and "course 3". For map zone records, it is always 1.</param>
     /// <param name="playerId">The player ID.</param>
     /// <param name="playerClass">The class. 3 is soldier, 4 is demoman.</param>
-    Task<PersonalRecordsModel> GetPersonalRecordAsync(string mapName, ZoneType zoneType, int zoneIndex, string playerId, Class playerClass);
+    Task<PersonalRecordsModel> GetPersonalRecordAsync(string mapName, ZoneType zoneType, int zoneIndex, string playerId, Class playerClass, CancellationToken cancellationToken = default);
 
     #endregion
 
@@ -139,21 +140,21 @@ public interface ITempusClient
     /// Get information about a player's rank
     /// </summary>
     /// <param name="playerId">The player ID.</param>
-    Task<Rank> GetPlayerRankAsync(long playerId);
+    Task<Rank> GetPlayerRankAsync(long playerId, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// /players/id/{playerId}/info
     /// Get basic player information
     /// </summary>
     /// <param name="playerId">The player ID.</param>
-    Task<PlayerInfo> GetPlayerInfoAsync(long playerId);
+    Task<PlayerInfo> GetPlayerInfoAsync(long playerId, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// /players/id/{playerId}/stats
     /// Get player statistics
     /// </summary>
     /// <param name="playerId">The player ID.</param>
-    Task<PlayerStatsModel> GetPlayerStatsAsync(long playerId);
+    Task<PlayerStatsModel> GetPlayerStatsAsync(long playerId, CancellationToken cancellationToken = default);
 
     #endregion
     
@@ -165,14 +166,14 @@ public interface ITempusClient
     /// </summary>
     /// <param name="class">The class. 3 is soldier, 4 is demoman.</param>
     /// <param name="start">Which rank to start the 50 from, default is 1</param>
-    Task<RanksOverviewModel> GetRanksAsync(int @class, int? start = null);
+    Task<RanksOverviewModel> GetRanksAsync(int @class, int? start = null, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// /ranks/overall
     /// List the top 50 players by overall ranking
     /// </summary>
     /// <param name="start">Which rank to start the 50 from, default is 1</param>
-    Task<RanksOverviewModel> GetTopOverallRanksAsync(int? start = null);
+    Task<RanksOverviewModel> GetTopOverallRanksAsync(int? start = null, CancellationToken cancellationToken = default);
     
     #endregion
     
@@ -183,7 +184,7 @@ public interface ITempusClient
     /// Get a record (run) overview by recordId
     /// </summary>
     /// <param name="recordId"></param>
-    Task<RunInfoModel> GetRecordAsync(long recordId);
+    Task<RunInfoModel> GetRecordAsync(long recordId, CancellationToken cancellationToken = default);
 
     #endregion
     
@@ -194,7 +195,7 @@ public interface ITempusClient
     /// Search for players and maps by name
     /// </summary>
     /// <param name="query">The search query. Case insensitive. Up to 20 players and 5 maps are returned.</param>
-    Task<PlayerMapSearchResult> GetSearchResultAsync(string query);
+    Task<PlayerMapSearchResult> GetSearchResultAsync(string query, CancellationToken cancellationToken = default);
 
     #endregion
 
@@ -205,13 +206,13 @@ public interface ITempusClient
     /// List up to 50 most recent demos for a server
     /// </summary>
     /// <param name="serverId">The server ID.</param>
-    Task<List<ServerDemoModel>> GetServerDemosAsync(long serverId);
+    Task<List<ServerDemoModel>> GetServerDemosAsync(long serverId, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// /servers/statusList
     /// List game server statuses
     /// </summary>
-    Task<List<ServerStatusModel>> GetServersStatusesAsync();
+    Task<List<ServerStatusModel>> GetServersStatusesAsync(CancellationToken cancellationToken = default);
 
     #endregion
 
@@ -226,7 +227,7 @@ public interface ITempusClient
     /// <param name="start">The record to start at. The default is 1.</param>
     /// <param name="sort">The field to sort on the. Working value is "duration"</param>
     /// <param name="direction">The direction to sort. Working values are "ascending"</param>
-    Task<ZonedRecordsModel> GetTopZonedTimes(long zoneId, long? limit = null, int? start = null, string sort = null, SortDirection? direction = null);
+    Task<ZonedRecordsModel> GetTopZonedTimes(long zoneId, long? limit = null, int? start = null, string sort = null, SortDirection? direction = null, CancellationToken cancellationToken = default);
 
     #endregion
 }
